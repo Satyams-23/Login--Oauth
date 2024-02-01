dccdd82ed5e148f8106a16ab2a602a0a215f211e
Satyam__Sharma <138864100+Satyams-23@users.noreply.github.com>
2024-02-01 19:51:03 +0530

    Update Instagram.js

--- Instagram/Instagram.js
+++ Instagram/Instagram.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 const app = express();
 const PORT = 8000;
 
-
+app.get('/', (req,res) => {
+    console.log("hello Guys")
+});
 
 // Instagram login route
 app.get('/login', (req, res) => {
